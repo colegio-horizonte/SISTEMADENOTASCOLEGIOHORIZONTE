d087832932ada6cba4d1f61b868bcc86f1a6ff61
colegio-horizonte <joaopedrofigueredodes@gmail.com>
2026-09-04 18:40:02 -0300

    Carregar gerador de provas v2

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
 (function(){
   function load(src){return new Promise(function(resolve){var s=document.createElement('script');s.src=src+'?v=20260904';s.onload=resolve;s.onerror=resolve;document.body.appendChild(s)})}
   try{Object.defineProperty(window,'__horizonte',{configurable:true,get:function(){return {get db(){return db}}}})}catch(e){window.__horizonte={get db(){return window.db}}}
-  load('/enhancements.js').then(function(){return load('/admin-tools.js')}).then(function(){return load('/nav-fix.js')});
+  load('/enhancements.js').then(function(){return load('/admin-tools.js')}).then(function(){return load('/nav-fix.js')}).then(function(){return load('/provas-v2.js')});
 })();
