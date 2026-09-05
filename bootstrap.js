faa15187b3962aff658989de6cb354a35a4e6409
colegio-horizonte <joaopedrofigueredodes@gmail.com>
2026-09-04 21:11:57 -0300

    Aplicar novo tema visual

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,7 @@
 (function(){
   function load(src){return new Promise(function(resolve){var s=document.createElement('script');s.src=src+'?v=20260904';s.onload=resolve;s.onerror=resolve;document.body.appendChild(s)})}
+  function css(href){var l=document.createElement('link');l.rel='stylesheet';l.href=href+'?v=20260904';document.head.appendChild(l)}
+  css('/visual-theme.css');
   try{Object.defineProperty(window,'__horizonte',{configurable:true,get:function(){return {get db(){return db}}}})}catch(e){window.__horizonte={get db(){return window.db}}}
   load('/enhancements.js').then(function(){return load('/admin-tools.js')}).then(function(){return load('/nav-fix.js')}).then(function(){return load('/provas-v2.js')});
 })();
